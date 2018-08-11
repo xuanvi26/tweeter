cadcc1bbf79f6dc910db8a1324e100c27838f5ee
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-08-10 20:26:19 -0400

    Change counter to red when limit exceeded

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     $('.new-tweet textarea').keyup(function() {
         let counter = 140 - $(this).val().length;
-        $(this).parent().find(".counter").html(`<span class="counter">${counter}</span>`);
+        let htmlCounter = $(this).parent().find(".counter")
+        htmlCounter.text(counter);
+        if(counter > 0) htmlCounter.css({"color": "#333"});
+        else htmlCounter.css({"color": "red"});
     })  
 });
