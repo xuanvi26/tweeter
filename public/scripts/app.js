a3064f5411aa34bb8aa9ca98a62d332373749a94
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-08-14 19:31:34 -0400

    Fetch tweets to server and apply hover after response

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,63 +7,6 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 
 $(document).ready(function() {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: {
-          small:
-            "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          regular:
-            "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          large:
-            "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        handle: "@SirIsaac"
-      },
-      content: {
-        text:
-          "If I have seen further it is by standing on the shoulders of giants"
-      },
-      created_at: 1461116232227
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: {
-          small:
-            "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          regular:
-            "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          large:
-            "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        handle: "@rd"
-      },
-      content: {
-        text: "Je pense , donc je suis"
-      },
-      created_at: 1461113959088
-    },
-    {
-      user: {
-        name: "Johann von Goethe",
-        avatars: {
-          small:
-            "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          regular:
-            "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          large:
-            "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        handle: "@johann49"
-      },
-      content: {
-        text: "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      created_at: 1461113796368
-    }
-  ];
 
   const createHeader = tweetData => {
     let header = $("<header>");
@@ -105,13 +48,27 @@ $(document).ready(function() {
     });
   };
 
-  renderTweets(data);
-
   $('.new-tweet form').submit(function (event) {
     event.preventDefault();
     const tweetBody = $('.new-tweet form textarea[name="text"]').val();
     $.post('/tweets', {text: tweetBody});
-    
   })
-  
+
+  const loadTweets = () => {
+    $.get('/tweets', function(data) {
+      renderTweets(data);
+      $("#tweet-container .tweet").hover(
+        function() {
+          $(this).css({ opacity: "1" });
+        },
+        function() {
+          $(this).css({ opacity: "0.6" });
+        }
+      );
+    })
+    console.log('This is after /tweets!')
+  }
+
+  loadTweets();
+
 });
