8ab17fdaa82ff06b33d1293a5aeaead9146dcf90
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-08-16 17:10:12 -0400

    Clean up comments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// Test / driver code (temporary). Eventually will get this from the server.
-
 $(document).ready(function() {
 
   const createHeader = tweetData => {
