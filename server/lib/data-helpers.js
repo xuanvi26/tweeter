736f6d9f2f35f1f2de82d86b55eddc916cc233c2
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-08-28 21:20:22 -0400

    Switch from in memory db to mongo db

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -6,22 +6,23 @@ const simulateDelay = require("./util/simulate-delay");
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection('tweets').insertOne(newTweet)
+      .then(result => callback(null, true))
+      .catch(err => callback(true, null))
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      db.collection("tweets")
+        .find()
+        .toArray((err, tweets) => {
+          if (err) {
+            return callback(err);
+          }
+          callback(null, tweets);
+        });
     }
-
   };
-}
+};
