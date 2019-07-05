de3a4d711bf1a92a91dcaff36296ff423589c6de
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2019-07-05 13:38:54 -0400

    Destructure mongo client instead

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app           = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const MongoClient = require("mongodb").MongoClient;
+const { MongoClient } = require("mongodb");
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
